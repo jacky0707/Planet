2e5ab2199e337b4b86de7bdee1d3e2f231107a99
songjie <songjie1120@g.ncu.edu.tw>
2018-10-04 15:00:07 +0800

    * maybe let the planet spin later

--- js/main.js
+++ js/main.js
@@ -29,16 +29,73 @@ function initDreams(){
         let handler = function(event){
             switch(event.target.id){
                 case "house":
-                if(document.getElementsByClassName('planet-house').length==5){
-                    drDreamTalk("full")
-                    event.target.disabled = true
-                }else{
-                    flyInToPlanet(event)
-                    dreamShowUp("house")
-                }
-                
-
-                break;
+                    if(document.getElementsByClassName('house-planet').length==5){
+                        drDreamTalk("full")
+                        event.target.disabled = true
+                    }else{
+                        flyInToPlanet(event)
+                        let chosenPlace = housePlace.pop()
+                        dreamShowUp("house").style.transform = "rotate("+chosenPlace+"deg)"
+                    }
+                    break;
+                case "car":
+                    if(document.getElementsByClassName('car-planet').length==10){
+                        drDreamTalk("full")
+                        event.target.disabled = true
+                    }else{
+                        flyInToPlanet(event)
+                        dreamShowUp("car")
+                    }
+                    break;
+                case "wedding":
+                    if(document.getElementsByClassName('wedding-planet').length==1){
+                        drDreamTalk("full")
+                        event.target.disabled = true
+                    }else{
+                        flyInToPlanet(event)
+                        let chosenPlace = weddingPlace.pop()
+                        dreamShowUp("wedding").style.transform = "rotate("+chosenPlace+"deg)"
+                    }
+                    break;
+                case "kid":
+                    if(document.getElementsByClassName('kid-planet').length==18){
+                        drDreamTalk("full")
+                        event.target.disabled = true
+                    }else{
+                        flyInToPlanet(event)
+                        let chosenPlace = kidPlace.pop()
+                        dreamShowUp("kid").style.transform = "rotate("+chosenPlace+"deg)"
+                    }
+                    break;
+                case "plane":
+                    if(document.getElementsByClassName('plane-planet').length==1){
+                        drDreamTalk("full")
+                        event.target.disabled = true
+                    }else{
+                        flyInToPlanet(event)
+                        dreamShowUp("plane")
+                    }
+                    break;
+                case "retire":
+                    if(document.getElementsByClassName('retire-planet').length==1){
+                        drDreamTalk("full")
+                        event.target.disabled = true
+                    }else{
+                        flyInToPlanet(event)
+                        let chosenPlace = retirePlace.pop()
+                        dreamShowUp("retire").style.transform = "rotate("+chosenPlace+"deg)"
+                    }
+                    break;
+                case "company":
+                    if(document.getElementsByClassName('company-planet').length==3){
+                        drDreamTalk("full")
+                        event.target.disabled = true
+                    }else{
+                        flyInToPlanet(event)
+                        let chosenPlace = companyPlace.pop()
+                        dreamShowUp("company").style.transform = "rotate("+chosenPlace+"deg)"
+                    }
+                    break;
             }
             
             
@@ -50,11 +107,12 @@ function dreamShowUp(dream){
     let amount = document.getElementsByClassName(dream+"-planet").length
     let newDream = createElement("DIV",{atrs:{
         id:dream+amount,
-        className:dream+"-planet "+"planet-things"
+        className:"planet-things "+dream+"-planet"
     }},document.getElementsByTagName("main")[0])
     setTimeout(function(){
         newDream.style.display="block"
-    },2200)
+    },2100)
+    return newDream
 }
 function drDreamTalk(type){
 
